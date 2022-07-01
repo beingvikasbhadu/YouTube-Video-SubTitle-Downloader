2a5dcb11ec5d3a4026b19a2b769b496ee8d42240
vikas bhadu <beingvikasbhadu@gmail.com>
2022-07-01 19:20:08 +0530

    Update and rename Subtitles Download of a youtube video.py to main.py

--- subtitile downloader/main.py
+++ subtitile downloader/main.py
@@ -2,17 +2,16 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 video_id=input("Enter Video Id: ")
 try:
-    transcript_list=YouTubeTranscriptApi.list_transcripts(video_id="vQQEaSnQ_bs")
+    transcript_list=YouTubeTranscriptApi.list_transcripts(video_id)
     transcript=transcript_list.find_transcript(['en'])
     list=transcript.fetch()
 
 # Save subtitles using file handling
     for Text in list:
-        with open('corey_schafer_subtitles.txt','a+') as f:
+        with open('corey_schafer_subtitles.txt','a+',encoding="utf-8") as f:
             f.write(Text['text'])
             f.write(' ')
         
 except Exception as e:
     print("Error:",e)
         
-
